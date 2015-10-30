8d9ccf9bfd4732f2c7041c89ea507e883f5e99bb
Ximena Montes Arellano <XimenaMArellano@Ximenas-MacBook-Pro.local>
2015-10-30 15:05:03 -0600

    IntentoDeHacerloCorrer

---
+++ Examen_/Assets/Scripts/Emisor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Emisor : MonoBehaviour {
+	public GameObject BlankCube;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//var myNewObject = Instantiate (BlankCube, Vector3(this.transform.position.x+pos, this.transform.position.y, this.transform.position.z)Quaternion.identity));
+		//myNewObject.renderer.material.color = Color.red;
+		float pos;
+		pos = Random.Range (-2, 2);
+		Vector3 newPos = new Vector3 (this.transform.position.x+pos, this.transform.position.y, this.transform.position.z );
+		//Instantiate (PelotaPrefab, this.transform.position, Quaternion.identity);
+		//var myNewObject = Instantiate (BlankCube, Vector3(x, y, z), Quaternion.identity)
+		Instantiate (BlankCube, newPos, Quaternion.identity);
+	
+	}
+}
